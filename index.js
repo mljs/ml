97e63aec7af820f2d1efbc2357adcd6fc482cff4
Daniel Kostro <kostro.d@gmail.com>
2015-01-27 11:01:01 +0100

    Add ml-stat

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ Core utilities
 Math
  */
 var Math = exports.Math = {};
-
 Math.Matrix = exports.Matrix = require('ml-matrix');
 Math.Distance = require('ml-distance');
+
+var Stat = exports.Stat = {};
+Stat.array = require('ml-stat/array');
+Stat.matrix = require('ml-stat/matrix');
 
 /*
 Neural networks
