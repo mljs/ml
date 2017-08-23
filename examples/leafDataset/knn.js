a6720d0306ee3ff882759cb28c0fa65ec23ec9c9
jajoe <joseph.lefevre@insa-rouen.fr>
2017-08-23 13:28:09 +0200

    feat: add an example of how to use classifiers

---
+++ examples/leafDataset/knn.js
@@ -0,0 +1,90 @@
+const KNN = require('ml-knn');
+const csv = require('csvtojson');
+const Matrix = require('ml-matrix');
+const PCA = require('ml-pca');
+
+let usePCA = false;
+
+let knn;
+
+const csvFilePath = 'leaf.csv'; // Data
+const names = ['type', 'specimenNumber', 'eccentricity', 'aspectRatio', 'elongation', 'solidity', 'stochasticConvexity', 'isoperimetricFactor', 'maxIndetationDepth', 'lobedness', 'intensity', 'contrast', 'smoothness', 'thirdMoment', 'uniformity', 'entropy']; // For header
+
+let seperationSize;
+let data = [], X = [], y = [];
+let trainingSetX = [], trainingSetY = [], testSetX = [], testSetY = [];
+
+csv({noheader: true, headers: names})
+    .fromFile(csvFilePath)
+    .on('json', (jsonObj) => {
+        data.push(jsonObj); // Push each object to data Array
+    })
+    .on('done', (error) => {
+        seperationSize = 0.9 * data.length;
+        data = shuffleArray(data);
+        dressData();
+    });
+
+function dressData() {
+    let types = new Set(); // To gather UNIQUE classes
+    data.forEach((row) => {
+        types.add(row.type);
+    });
+    typesArray = [...types]; // To save the different types of classes.
+
+    data.forEach((row) => {
+        let rowArray, typeNumber;
+        rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(2, 16); // We don't use the 2 first elements, which are the type (i.e class) and the specimen number (i.e ID)
+        typeNumber = typesArray.indexOf(row.type); // Convert type(String) to type(Number)
+
+        X.push(rowArray);
+        y.push(typeNumber);
+    });
+
+    trainingSetX = X.slice(0, seperationSize);
+    trainingSetY = y.slice(0, seperationSize);
+    testSetX = X.slice(seperationSize);
+    testSetY = y.slice(seperationSize);
+
+    train();
+}
+
+function train() {
+    if (usePCA === true) {
+        trainingSetX = new Matrix(trainingSetX);
+        testSetX = new Matrix(testSetX);
+        var pca = new PCA(trainingSetX);
+        trainingSetX = pca.predict(trainingSetX, 2);
+        testSetX = pca.predict(testSetX, 2);
+    }
+    knn = new KNN(trainingSetX, trainingSetY, {k:1});
+    test();
+}
+
+function test() {
+    const result = knn.predict(testSetX);
+    const testSetLength = testSetX.length
+    const predictionError = error(result, testSetY);
+    console.log(`Test Set Size = ${testSetLength} and number of Misclassifications = ${predictionError}`);
+}
+
+function error(predicted, expected) {
+    let misclassifications = 0;
+    for (var index = 0; index < predicted.length; index++) {
+        console.log(`truth : ${expected[index]} and prediction : ${predicted[index]}`);
+        if (predicted[index] !== expected[index]) {
+            misclassifications++;
+        }
+    }
+    return misclassifications;
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
