9cf0471c1c2787b136f6e1ad537fee20abc02a39
Luc Patiny <lpatiny@users.noreply.github.com>
2024-10-21 08:58:41 +0200

    feat!: update dependencies (#186)
    It is always advisable to import individual packages, as it is much easier to track the breaking changes. 
    Because we updated all the dependencies, numerous bug fixes, features, and breaking changes have been incorporated.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,17 @@
 /* eslint-disable import/order */
 /* eslint-disable import/first */
 
-import { DecisionTreeClassifier, DecisionTreeRegression } from 'ml-cart';
-import {
-  RandomForestClassifier,
-  RandomForestRegression,
-} from 'ml-random-forest';
-
 // Try to keep this list in the same structure as the README.
 
 // Unsupervised learning
 export { PCA } from 'ml-pca';
-import * as HClust from 'ml-hclust';
-export { HClust };
-import * as KMeans from 'ml-kmeans';
-export { KMeans };
 
 // Supervised learning
-import * as NaiveBayes from 'ml-naivebayes';
-export { NaiveBayes };
+
 export { default as KNN } from 'ml-knn';
 export { PLS, KOPLS, OPLS, oplsNipals } from 'ml-pls';
-import * as CrossValidation from 'ml-cross-validation';
-export { CrossValidation };
+
 export { ConfusionMatrix } from 'ml-confusion-matrix';
-export { DecisionTreeClassifier };
-export { RandomForestClassifier };
 
 // Artificial neural networks
 export { default as FNN } from 'ml-fnn';
@@ -42,13 +28,9 @@ export {
   TheilSenRegression,
   RobustPolynomialRegression,
 } from 'ml-regression';
-export { DecisionTreeRegression };
-export { RandomForestRegression };
 
 // Optimization
 export { levenbergMarquardt } from 'ml-levenberg-marquardt';
-import * as FCNNLS from 'ml-fcnnls';
-export { FCNNLS };
 
 // Math
 import * as MatrixLib from 'ml-matrix';
@@ -61,7 +43,6 @@ const {
   QrDecomposition,
 } = MatrixLib;
 export {
-  MatrixLib,
   Matrix,
   SVD,
   EVD,
@@ -72,10 +53,9 @@ export {
 
 export { SparseMatrix } from 'ml-sparse-matrix';
 export { default as Kernel } from 'ml-kernel';
-import { distance, similarity } from 'ml-distance';
-export { distance as Distance, similarity as Similarity };
+export { distance as Distance, similarity as Similarity } from 'ml-distance';
 export { default as distanceMatrix } from 'ml-distance-matrix';
-export { default as XSadd } from 'ml-xsadd';
+export { XSadd } from 'ml-xsadd';
 export { nGMCA } from 'ml-ngmca';
 
 // Statistics
@@ -89,9 +69,7 @@ export { default as BitArray } from 'ml-bit-array';
 export { default as HashTable } from 'ml-hash-table';
 export { default as padArray } from 'ml-pad-array';
 export { default as binarySearch } from 'binary-search';
-export { default as Random } from 'ml-random';
-import * as GSD from 'ml-gsd';
-export { GSD };
+export { Random } from 'ml-random';
 
 import min from 'ml-array-min';
 import max from 'ml-array-max';
@@ -140,3 +118,15 @@ export const ArrayXY = {
   equallySpaced,
   filterX,
 };
+export { DecisionTreeClassifier, DecisionTreeRegression } from 'ml-cart';
+export {
+  RandomForestClassifier,
+  RandomForestRegression,
+} from 'ml-random-forest';
+export * as HClust from 'ml-hclust';
+export * as KMeans from 'ml-kmeans';
+export * as NaiveBayes from 'ml-naivebayes';
+export * as CrossValidation from 'ml-cross-validation';
+export * as FCNNLS from 'ml-fcnnls';
+export * as MatrixLib from 'ml-matrix';
+export * as GSD from 'ml-gsd';
